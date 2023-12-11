776842623125f5cba06494aafb738d1862fc190a
ssy <1161209667@qq.com>
2023-12-11 19:04:14 +0800

    创建项目

---
+++ Assets/FPS/Scripts/Chushengdian/CreateDiren.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateDiren : MonoBehaviour
+{
+    public int yanshiTime;//延时时间
+    public int count;//总数量
+
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
