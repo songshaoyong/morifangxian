d336b1165453595f8da29c5c1cc7acc173324624
ssy <1161209667@qq.com>
2023-12-13 19:19:35 +0800

    添加了敌人

--- Assets/FPS/Scripts/AI/TestXulu.cs
+++ Assets/FPS/Scripts/AI/TestXulu.cs
@@ -5,18 +5,55 @@ using UnityEngine.AI;
 
 public class TestXulu : MonoBehaviour
 {
-    public NavMeshAgent nav;
-    public Transform target;
+    //    public NavMeshAgent nav;
+    public Transform[] target;
+    private int currentTargetIndex = 0;
+    public float maxOffset = 1.0f; // 最大的左右偏移量
+    private NavMeshAgent navMeshAgent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.nav.SetDestination(this.target.position);
+       // navMeshAgent.SetDestination(this.target[currentTargetIndex].position);
+        if (!navMeshAgent.pathPending && navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        {
+            OnPathComplete(); // 手动触发事件
+        }
+    }
 
+    // 用于设置目标对象的方法
+    public void SetTarget(Transform[] newTarget)
+    {
+        target = newTarget;
+    }
+
+    // 到达目标点时调用的方法
+    void OnPathComplete()
+    {
+        Debug.Log("Enemy reached the destination!");
+
+        // 到达目标后切换到下一个目标点
+        currentTargetIndex = (currentTargetIndex + 1) % target.Length;
+        if (currentTargetIndex == target.Length - 1)
+        {
+            SetRandomDestination(0); // 到达目标后重新设置随机目标位置
+        }
+        else
+        {
+            SetRandomDestination(maxOffset); // 到达目标后重新设置随机目标位置
+        }
+       
+    }
+    // 设置随机目标位置
+    void SetRandomDestination(float maxOffset)
+    {
+        Vector3 randomOffset = new Vector3(Random.Range(-maxOffset, maxOffset), 0f, Random.Range(-maxOffset, maxOffset));
+        Vector3 newDestination = target[currentTargetIndex].position + randomOffset;
+        navMeshAgent.SetDestination(newDestination);
     }
 }
--- Assets/FPS/Scripts/Chushengdian/CreateDiren.cs
+++ Assets/FPS/Scripts/Chushengdian/CreateDiren.cs
@@ -4,19 +4,48 @@ using UnityEngine;
 
 public class CreateDiren : MonoBehaviour
 {
-    public int yanshiTime;//延时时间
-    public int count;//总数量
+    public GameObject enemyPrefab; // 敌人的预制体
+    public Transform spawnPoint; // 出生点
+    public Transform[] targetPoints; // 多个目标点
+    public float timeBetweenWaves = 5f; // 每波之间的间隔时间
+    public int[] numberOfEnemiesInWaves = { 5, 10, 15 }; // 每波的敌人数量
+    private int currentWave = 0;
 
- 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnWaves());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnWaves()
     {
+        while (currentWave < numberOfEnemiesInWaves.Length)
+        {
+            for (int i = 0; i < numberOfEnemiesInWaves[currentWave]; i++)
+            {
+                SpawnEnemy();
+                yield return new WaitForSeconds(1f); // 生成敌人之间的间隔时间
+            }
+
+            yield return new WaitForSeconds(timeBetweenWaves); // 波次之间的间隔时间
+            currentWave++;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        //   Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+        Vector3 spawnPosition = spawnPoint.position + new Vector3(Random.Range(-2f, 2f), 0f, Random.Range(-2f, 2f));
+
+        GameObject enemy = Instantiate(enemyPrefab, spawnPosition, spawnPoint.rotation);
+        TestXulu enemyScript = enemy.GetComponent<TestXulu>();
+        enemyScript.SetTarget(targetPoints);
+       /* if (enemyScript != null)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+            // 将玩家对象设置为敌人的目标
+            enemyScript.SetTarget(player.transform);
         
+        }*/
     }
 }
